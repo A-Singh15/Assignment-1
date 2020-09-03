5f3e97ada267a0a4da633189cf4c0fbc54b3937d
A-Singh15 <asingh39@sfsu.edu>
2020-09-03 15:26:11 -0700

    Updated HelloWorld that complies the version that prof souza gave us was filled with error.

--- HelloWorld.c
+++ HelloWorld.c
@@ -8,21 +8,14 @@
 // This specific code in coding language C outputs Hello World in uppercase
 // Changes to the code from the original code. The "hello world!" from the file was lowercase but I have changed it to "Hello World!" uppercase
 
-// ------------------------------------------------------------------------------------------------------------------------------------
-
-void // used as a function return type 
-super_print() //superclass refers to the parent objects 
-{  // Open Curly bracket for the main class to add a certain  
-	printf("%s\n", "Wello World!"); // prints the given values in the parameters"..."
-} // Closed Curly Bracket for the ending of given main class
-
-int // int is short for integers to recoginze number values or in this clase a variable buit into the compiler 
-main()  //  entry point of the first function  for the program to be executed
- { // Open Curly bracket for the return point to return the given main class 
-	return 0;
-}  // Closed Curly bracket for the ending return point to output the given code
-
-
-// ---------------------------------------------------------------------------------------------
-
----------------------------------------
+#include <stdio.h>
+ 
+ int  // int is short for integers to recoginze number values or in this clase a variable buit into the compiler 
+ main()  // entry point of the first function  for the program to be executed
+ 
+ {  // Open Curly bracket for the main class to add a certain
+ printf("Hello, World!");
+ return 0;
+ 
+     
+ }// Closed Curly Bracket for the ending of given main class
