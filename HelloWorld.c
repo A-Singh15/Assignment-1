789e5bd0d9a08862445e8b7823e6592a40e3239d
A-Singh15 <asingh39@sfsu.edu>
2020-09-03 11:25:58 -0700

    This is the test HelloWorld.c file with documentation and source cited.

---
+++ HelloWorld.c
@@ -0,0 +1,28 @@
+// This forked file project Author: Aaryan Singh
+// Documentation written by : Aaryan Singh 
+// Class CSC-413-02
+// Professor: Anthony J Souza
+// Referenced author Prof Souza in the github and ilearn
+// Orginal credit to git repo to : https://github.com/asouza88/lecture-demo
+// What I learned in this demo lecture assignment is the Uses of github,how to commit,how to make new files,how to make nxew ////////directorys,and mainly get ready for the first assignment on ilearn.
+// This specific code in coding language C outputs Hello World in uppercase
+// Changes to the code from the original code. The "hello world!" from the file was lowercase but I have changed it to "Hello World!" uppercase
+
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+void // used as a function return type 
+super_print() //superclass refers to the parent objects 
+{  // Open Curly bracket for the main class to add a certain  
+	printf("%s\n", "Wello World!"); // prints the given values in the parameters"..."
+} // Closed Curly Bracket for the ending of given main class
+
+int // int is short for integers to recoginze number values or in this clase a variable buit into the compiler 
+main()  //  entry point of the first function  for the program to be executed
+ { // Open Curly bracket for the return point to return the given main class 
+	return 0;
+}  // Closed Curly bracket for the ending return point to output the given code
+
+
+// ---------------------------------------------------------------------------------------------
+
+---------------------------------------
